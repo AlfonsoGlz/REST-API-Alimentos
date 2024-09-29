c2f5122bc6b698f8d030fd0064df5fb09c869e5c
AlfonsoGlz <alfonsocornejo8085@gmail.com>
2024-09-29 01:50:49 -0600

    Registro de servicio de DbContext

--- RESTAPI_Alimentos/Program.cs
+++ RESTAPI_Alimentos/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using RESTAPI_Alimentos.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+//Registrando servicio de DbContext para inyeccion de dependencias
+builder.Services.AddDbContext<API_AlimentosContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConexionDefault")));
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
