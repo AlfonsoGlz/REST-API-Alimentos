c4bbdb3eeffe5e041a99df6285bab9fee9215954
AlfonsoGlz <alfonsocornejo8085@gmail.com>
2024-09-29 14:52:53 -0600

    Metodos edit y delete

--- RESTAPI_Alimentos/Controllers/AlimentosController.cs
+++ RESTAPI_Alimentos/Controllers/AlimentosController.cs
@@ -36,6 +36,40 @@ namespace RESTAPI_Alimentos.Controllers
             return Ok();  
         }
 
+        [HttpPut]
+        [Route("editar_alimento")]
+        public async Task<IActionResult> EditarAlimento(Alimento a)
+        {
+            var alimentoExsistente = await _context.Alimentos.FindAsync(a.IdAlimentos);
+
+            alimentoExsistente!.NombreAlimento = a.NombreAlimento;
+            alimentoExsistente.Cantidad = a.Cantidad;
+            alimentoExsistente.Calorias = a.Calorias;
+            alimentoExsistente.Proteinas = a.Proteinas;
+            alimentoExsistente.Carbohidratos = a.Carbohidratos;
+            alimentoExsistente.Grasas = a.Grasas;
+            alimentoExsistente.Fibra = a.Fibra;
+            alimentoExsistente.Sodio = a.Sodio;
+            alimentoExsistente.CategoriaId = a.CategoriaId;
+            alimentoExsistente.UnidadId = a.UnidadId;
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("eliminar_alimento/{id}")]
+        public async Task<IActionResult> EliminarAlimento(int id)
+        {
+            var alimento = await _context.Alimentos.FindAsync(id);
+            _context.Alimentos.Remove(alimento!);
+
+            await _context.SaveChangesAsync();
+            return Ok();
+
+        }
+
 
 
 
