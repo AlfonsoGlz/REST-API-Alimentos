5c09f81e98e7d9784a63d5ea1d5819753f6d8338
AlfonsoGlz <alfonsocornejo8085@gmail.com>
2024-10-06 14:39:30 -0600

    Agrega cliente

---
+++ Cliente_REST_Alimentos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cliente_REST_Alimentos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
