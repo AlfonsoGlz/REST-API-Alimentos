b92ae26db14a81df7ba91e7267bf422b3d7a7398
AlfonsoGlz <alfonsocornejo8085@gmail.com>
2024-10-06 02:00:54 -0600

    Se agrega controlador Categorias con metodos GET y POST

---
+++ RESTAPI_Alimentos/Controllers/CategoriasController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RESTAPI_Alimentos.Models;
+
+namespace RESTAPI_Alimentos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly API_AlimentosContext _context;
+
+        public CategoriasController(API_AlimentosContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("listar_categorias")]
+        public async Task<IActionResult> ObtenerCategorias()
+        {
+            Task<List<Categoria>> lsCategorias = _context.Categorias.ToListAsync();
+
+            return Ok(await lsCategorias);
+        }
+
+        [HttpPost]
+        [Route("agregar_categoria")]
+        public async Task<IActionResult> AgregarCategoria(Categoria c)
+        {
+            await _context.Categorias.AddAsync(c);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+
+    }
+}
