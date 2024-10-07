950a7f7316ae38b755334ce2aa6b6fb17c346f24
AlfonsoGlz <alfonsocornejo8085@gmail.com>
2024-10-07 00:28:26 -0600

    Se agrga servicio HttpClient

--- Cliente_REST_Alimentos/Program.cs
+++ Cliente_REST_Alimentos/Program.cs
@@ -3,6 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//Agregamos servico httpclient para conectarse a la web api
+builder.Services.AddHttpClient();
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,6 +23,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Alimentos}/{action=Index}/{id?}");
 
 app.Run();
