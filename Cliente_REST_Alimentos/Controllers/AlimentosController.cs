7e7607a01d2a5a1d7f41055744402f06dfc21601
AlfonsoGlz <alfonsocornejo8085@gmail.com>
2024-10-13 01:33:29 -0600

    Modificacion

--- Cliente_REST_Alimentos/Controllers/AlimentosController.cs
+++ Cliente_REST_Alimentos/Controllers/AlimentosController.cs
@@ -16,14 +16,34 @@ namespace Cliente_REST_Alimentos.Controllers
             _httpClient.BaseAddress = new Uri("http://localhost:63877/api");
         }
 
+        //public async Task<IActionResult> Index()
+        //{
+        //    var lsCategorias = new List<CategoriaCl>();
+
+        //    HttpResponseMessage request = await _httpClient.GetAsync("api/Categorias/listar_categorias");
+
+        //    if (request.IsSuccessStatusCode)
+        //    {
+        //        string result = await request.Content.ReadAsStringAsync();
+        //        lsCategorias = JsonConvert.DeserializeObject<List<CategoriaCl>>(result);
+        //    }
+        //    else
+        //    {
+        //        throw new Exception("Error de comunicacion");
+        //    }
+
+        //    return View(lsCategorias);
+        //}
+
+
         //Metodo que muestra la la lista entera de alimentos
         public async Task<IActionResult> Index()
         {
             List<AlimentosCl>? lsAlimentos = new List<AlimentosCl>();
 
             HttpResponseMessage request = await _httpClient.GetAsync("api/Alimentos/listar_todo");
-            
-            if(request.IsSuccessStatusCode)
+
+            if (request.IsSuccessStatusCode)
             {
                 string result = await request.Content.ReadAsStringAsync();
                 lsAlimentos = JsonConvert.DeserializeObject<List<AlimentosCl>>(result);
